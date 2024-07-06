5fde776c2bf0a52fe9f51c1a3f144dd1d00ffb19
Abhishek Gond <abhishekgond9670@gmail.com>
2024-07-06 21:10:26 +0530

    testing on network

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @socketio.on('message')
-def handle_message(msg):
-    print(f'Message: {msg}')
+def handleMessage(msg):
+    print('Message: ' + msg)
     send(msg, broadcast=True)
 
 if __name__ == '__main__':
